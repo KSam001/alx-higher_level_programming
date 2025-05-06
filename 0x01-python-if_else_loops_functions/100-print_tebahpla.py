4ab711f00593a9c97df2416605c1ad37950ef1da
KSam001 <samcapital.inc@gmail.com>
2025-05-06 00:15:33 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(25, -1, -1):
-    print("{:c}".format(122 - (i % 2) * 32 if i % 2 == 0 else 89 + i // 2), end="")
+for i in range(0, 26):
+    print("{:c}".format(122 - i if i % 2 == 0 else 90 - i // 2), end="")
